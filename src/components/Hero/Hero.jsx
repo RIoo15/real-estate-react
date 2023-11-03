712b498c85323d8fc7bedca8554969d0df6959a5
RIoo15 <hashim74100@gmail.com>
2023-11-03 15:14:32 +0500

    SEARCH BAR RESPONSE

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
             </span>
           </div>
           <div className="flexCenter search-bar">
-            <HiLocationMarker color="var(--blue)" size={25} />
+            <HiLocationMarker className="icon1" color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
           </div>
